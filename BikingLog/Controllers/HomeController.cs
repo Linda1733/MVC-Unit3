e7cc8667f6346f0228e53d7776c57b5f7881427c
LindaM <l_m_mull@yahoo.com>
2019-10-05 20:46:55 -0400

    Fixed css

--- BikingLog/Controllers/HomeController.cs
+++ BikingLog/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace BikingLog.Controllers
 
                 new Trail {TrailID = 1, Name = "Cumberland Valley Trail", Location="Newville", Miles=21, DifficultyLevel = 4},
                 new Trail {TrailID = 2, Name = "Conewago Recreation Trail", Location="Elizabethtown", Miles=5,  DifficultyLevel = 2},
-                new Trail {TrailID = 1, Name = "Jonathan Eshenour Memorial Trail", Location="Hummelstown", Miles=13, DifficultyLevel = 3},
-                new Trail {TrailID = 2, Name = "Heritage Rail Trail", Location="York", Miles=21,  DifficultyLevel = 4},
-                new Trail {TrailID = 1, Name = "Capital Area Greenbelt", Location="Harrisburg", Miles=20, DifficultyLevel = 4},
-                new Trail {TrailID = 2, Name = "Lebanon Valley Rail Trail", Location="Lebanon", Miles=12,  DifficultyLevel = 3}
+                new Trail {TrailID = 3, Name = "Jonathan Eshenour Memorial Trail", Location="Hummelstown", Miles=13, DifficultyLevel = 3},
+                new Trail {TrailID = 4, Name = "Heritage Rail Trail", Location="York", Miles=21,  DifficultyLevel = 4},
+                new Trail {TrailID = 5, Name = "Capital Area Greenbelt", Location="Harrisburg", Miles=20, DifficultyLevel = 4},
+                new Trail {TrailID = 6, Name = "Lebanon Valley Rail Trail", Location="Lebanon", Miles=12,  DifficultyLevel = 3}
 
             };
 
