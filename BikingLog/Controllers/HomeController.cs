c75d16dd6631816c27f7d363a11aadf659f13f2a
LindaM <l_m_mull@yahoo.com>
2019-10-05 20:37:16 -0400

    Finished project

---
+++ BikingLog/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BikingLog.Models;
+
+using System.Threading.Tasks;
+
+namespace BikingLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+
+            Trail[] trailArray =
+            {
+
+                new Trail {TrailID = 1, Name = "Cumberland Valley Trail", Location="Newville", Miles=21, DifficultyLevel = 4},
+                new Trail {TrailID = 2, Name = "Conewago Recreation Trail", Location="Elizabethtown", Miles=5,  DifficultyLevel = 2},
+                new Trail {TrailID = 1, Name = "Jonathan Eshenour Memorial Trail", Location="Hummelstown", Miles=13, DifficultyLevel = 3},
+                new Trail {TrailID = 2, Name = "Heritage Rail Trail", Location="York", Miles=21,  DifficultyLevel = 4},
+                new Trail {TrailID = 1, Name = "Capital Area Greenbelt", Location="Harrisburg", Miles=20, DifficultyLevel = 4},
+                new Trail {TrailID = 2, Name = "Lebanon Valley Rail Trail", Location="Lebanon", Miles=12,  DifficultyLevel = 3}
+
+            };
+
+            return View(trailArray);
+        }
+    }
+}
---
+++ BikingLog/Models/Trail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikingLog.Models
+{
+    public class Trail
+    {
+        public int TrailID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Miles { get; set; }
+        public int DifficultyLevel { get; set; }
+    }
+}
